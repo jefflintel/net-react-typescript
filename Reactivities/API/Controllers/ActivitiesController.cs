a1d9cb0e0f99da38966bca51211dcfd54bae960f
Jeff Lintel <jefflintel@gmail.com>
2024-05-14 13:10:12 -0500

    added Identity/backend auth

--- Reactivities/API/Controllers/ActivitiesController.cs
+++ Reactivities/API/Controllers/ActivitiesController.cs
@@ -1,5 +1,6 @@
 using Application.Activities;
 using Domain;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -12,31 +13,30 @@ namespace API.Controllers
         {
             return HandleResult(await Mediator.Send(new List.Query()));
         }
-
         [HttpGet("{id}")] // api/activities/${guid}
         public async Task<IActionResult> GetActivity(Guid id)
         {
-            return HandleResult(await Mediator.Send(new Details.Query{Id = id}));
+            return HandleResult(await Mediator.Send(new Details.Query { Id = id }));
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateActivity(Activity activity) 
+        public async Task<IActionResult> CreateActivity(Activity activity)
         {
-           return HandleResult(await Mediator.Send(new Create.Command{Activity = activity}));
-           
+            return HandleResult(await Mediator.Send(new Create.Command { Activity = activity }));
+
         }
 
         [HttpPut("{id}")]
         public async Task<IActionResult> EditActivity(Guid id, Activity activity)
         {
             activity.Id = id;
-            return HandleResult(await Mediator.Send(new Edit.Command {Activity = activity}));
+            return HandleResult(await Mediator.Send(new Edit.Command { Activity = activity }));
         }
 
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteActivity(Guid id) 
+        public async Task<IActionResult> DeleteActivity(Guid id)
         {
-            return HandleResult(await Mediator.Send(new Delete.Command{Id = id}));             
+            return HandleResult(await Mediator.Send(new Delete.Command { Id = id }));
         }
     }
 }
--- Reactivities/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Reactivities/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5cad7b3be806081bbe0f3d8396140df561910e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9de50b56064869511b4ae501d5a44494d325d0ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
